22d192750089499736b81380d5632400f8a18064
Brian Turchyn <brian@dj-bri-t.net>
2011-05-26 11:30:53 -0500

    Added SQL for populating eras table

---
+++ src/install/tabledata/101-populate-table-eras.sql
@@ -0,0 +1,25 @@
+INSERT INTO %seras (
+  `era_id`,
+  `era_name`,
+  `deleted_dt`
+)
+VALUES (
+  NULL,
+  '1890s',
+  NULL
+),
+(
+  NULL,
+  '1920s',
+  NULL
+),
+(
+  NULL,
+  '1990s',
+  NULL
+),
+(
+  NULL,
+  'Delta Green',
+  NULL
+);

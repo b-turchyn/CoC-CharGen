8cbc7e2a90506d6784d4f0feccc5f9ae3b3f5ac8
Brian Turchyn <brian@dj-bri-t.net>
2011-05-26 11:11:53 -0500

    Added origin and meaning table columns for names

--- src/install/tabledata/002-create-table-names.sql
+++ src/install/tabledata/002-create-table-names.sql
@@ -4,6 +4,8 @@ CREATE TABLE IF NOT EXISTS %snames (
 	`gender` CHAR(1) NOT NULL DEFAULT 'B' COMMENT 'M for Male, F for Female, B for Both',
 	`isfirst` BOOL NOT NULL DEFAULT '1' COMMENT 'True for first name, false for last name',
 	`era_id` INT NOT NULL COMMENT 'Reference to applicable era',
+        `meaning` VARCHAR(255) NOT NULL DEFAULT "",
+        `origin` VARCHAR(100) NOT NULL,
 	`deleted_dt` TIMESTAMP NULL DEFAULT NULL,
 	INDEX (
 		`gender`,

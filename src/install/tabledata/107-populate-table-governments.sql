90bba92f71824896a1fc465229fa10ca9f4a79ce
Brian Turchyn <brian@dj-bri-t.net>
2011-05-26 14:03:34 -0500

    Fixed countries content. Added governments content

---
+++ src/install/tabledata/107-populate-table-governments.sql
@@ -0,0 +1,7 @@
+INSERT INTO %sgovernments (`government_id`, `name`, `description`, `deleted_dt`) VALUES
+(1, 'Independent State', NULL, NULL),
+(2, 'Antarctic Territory', NULL, NULL),
+(3, 'Dependency', NULL, NULL),
+(4, 'Disputed Territory', NULL, NULL),
+(5, 'Proto Dependency', NULL, NULL),
+(6, 'Proto Independent State', NULL, NULL);

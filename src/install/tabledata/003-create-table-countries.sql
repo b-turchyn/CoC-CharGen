39c30c46f05e0815723c756f28aa13cce7c2f49d
Brian Turchyn <brian@dj-bri-t.net>
2011-05-26 13:12:55 -0500

    Adding initial table contents to countries table

--- src/install/tabledata/003-create-table-countries.sql
+++ src/install/tabledata/003-create-table-countries.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IF NOT EXISTS %scountries (
 	`country_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	`name` VARCHAR(64) NOT NULL,
+        `code` VARCHAR(3) NOT NULL,
 	`population` BIGINT(15) NOT NULL,
 	`era_id` INT NOT NULL,
 	`government_id` INT NOT NULL,
+        `city_id` INT NOT NULL COMMENT 'Capital city',
 	`deleted_dt` TIMESTAMP NULL DEFAULT NULL,
 	INDEX (
 		`name`,

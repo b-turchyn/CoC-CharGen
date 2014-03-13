9288580897e10a656d853bdf0ccb931d20dfedd5
Brian Turchyn <brian@dj-bri-t.net>
2014-03-12 21:54:34 -0500

    Forgot to add Occupation model in fe01d471

---
+++ src/models/occupation.php
@@ -0,0 +1,55 @@
+<?php
+/************************************************************************
+ * Call of Cthulhu Character Generator
+ * Copyright (C) 2014 Brian Turchyn
+ * All references to commercial items copyright their respective owners.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ************************************************************************/
+
+class Occupation
+{
+  private static $occupations = NULL;
+
+  static function getOccupations( ) {
+    global $sql;
+
+    if( self::$occupations === NULL ) {
+      self::$occupations = $sql->getOccupations( );
+      array_unshift( self::$occupations, array( 'key' => 'R', 'value' => 'Random' ) );
+    }
+
+    return self::$occupations;
+  }
+
+  static function getValue( $occupation ) {
+    $result = get( $occupation );
+
+    return ( $result != NULL ? $result['value'] : NULL );
+  }
+
+  static function get( $occupation ) {
+    $result = NULL;
+
+    foreach ( self::getOccupations( ) as $value ) {
+      if( $value['key'] == $occupation ) {
+        $result = $value;
+        break;
+      }
+    }
+
+    return $result;
+  }
+}
+?>
